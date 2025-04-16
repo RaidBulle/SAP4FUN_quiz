d07e44f85592b6fdb9edbe091e9fdb89a91bab4b
RaidBulle <unamburel@gmail.com>
2025-04-16 09:38:57 +0200

    ok

---
+++ script.js
@@ -0,0 +1,17 @@
+const questions = [
+    {
+        question: "Quel est l'outil de versionnement le plus utilisé ?",
+        options: ["Git", "SVN", "Mercurial"],
+        answer: 0
+    }
+    // Ajoute tes 1400 questions ici plus tard !
+];
+
+document.getElementById('start-btn').addEventListener('click', () => {
+    startQuiz();
+});
+
+function startQuiz() {
+    // Logique du quiz à venir
+    alert("Quiz démarré !");
+}
