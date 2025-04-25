fa9128c3fd1dc1905b6862bd7cb64d9f5e037382
RaidBulle <unamburel@gmail.com>
2025-04-25 15:41:33 +0200

    Update and rename domUtils.js to dom-utils.js

---
+++ js/dom-utils.js
@@ -0,0 +1,37 @@
+export function createElement(tag, attributes = {}, children = []) {
+    const element = document.createElement(tag);
+    
+    Object.entries(attributes).forEach(([key, value]) => {
+        if (key === 'class') {
+            element.className = value;
+        } else if (key.startsWith('data-')) {
+            element.setAttribute(key, value);
+        } else {
+            element[key] = value;
+        }
+    });
+
+    children.forEach(child => {
+        if (typeof child === 'string') {
+            element.appendChild(document.createTextNode(child));
+        } else {
+            element.appendChild(child);
+        }
+    });
+
+    return element;
+}
+
+export function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+export function disableElements(...elements) {
+    elements.forEach(el => el.disabled = true);
+}
+
+export function enableElements(...elements) {
+    elements.forEach(el => el.disabled = false);
+}
