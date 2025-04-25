f073e8e097f3f9e883482a09a317fda38302f49c
RaidBulle <unamburel@gmail.com>
2025-04-25 10:57:06 +0200

    Update dataloader.js

--- js/dataloader.js
+++ js/dataloader.js
@@ -1,4 +1,18 @@
-// main.js
-window.addEventListener('DOMContentLoaded', () => {
-    loadQuestions();
-});
+// dataLoader.js
+let questionsData = [];
+
+function loadQuestions() {
+    fetch('data/questions.json')
+        .then(response => {
+            if (!response.ok) throw new Error('Erreur de réseau');
+            return response.json();
+        })
+        .then(data => {
+            questionsData = data;
+            initDomaines();
+        })
+        .catch(error => {
+            console.error("Erreur lors du chargement des questions:", error);
+            alert("Erreur lors du chargement des questions. Veuillez recharger la page.");
+        });
+}
